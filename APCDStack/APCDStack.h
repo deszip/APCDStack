58a61d9ece61994c36c58cda54d399e394b1661f
Deszip <raf.rafiki@gmail.com>
2015-05-20 10:14:51 +0300

    Initial Commit

---
+++ APCDStack/APCDStack.h
@@ -0,0 +1,19 @@
+//
+//  APCDStack.h
+//  APCDStack
+//
+//  Created by Deszip on 20/05/15.
+//  Copyright (c) 2015 Alterplay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for APCDStack.
+FOUNDATION_EXPORT double APCDStackVersionNumber;
+
+//! Project version string for APCDStack.
+FOUNDATION_EXPORT const unsigned char APCDStackVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <APCDStack/PublicHeader.h>
+
+
